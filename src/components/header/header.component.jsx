bf1e663128f6fd280ae1ed1c038576d182896ef7
Sarah <sarahjune@gmail.com>
2021-11-12 21:30:15 +1100

    header font logo thing

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,10 +11,10 @@ const Header = ({ currentUser }) => (
       <Logo className="logo" />
     </Link>
     <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
+      <Link className="option" to="/">
+        ABOUT
       </Link>
-      <Link className="option" to="/shop">
+      <Link className="option" to="/">
         CONTACT
       </Link>
       {currentUser ? (
