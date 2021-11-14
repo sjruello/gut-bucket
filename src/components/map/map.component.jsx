770a9d297a95037f6774e2ddc67fae35d35b8900
Alex Poulsen <lexpoulsen@gmail.com>
2021-11-14 13:44:07 +1100

    Map added to Trip component

--- src/components/map/map.component.jsx
+++ src/components/map/map.component.jsx
@@ -1,12 +1,47 @@
 import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import mapStyles from './mapStyles';
 
 import "./map.styles.scss";
 
 // TODO: fix this
-const Map = ({ location, zoomLevel }) => (
-  <div className="map">
-    <h2 className="map-h2">MAP GO HERE</h2>
-  </div>
-);
+const Map = ({ location, zoomLevel }) => {
+
+  const libraries = ["places"];
+  const mapContainerStyle = {
+    width: '45vw',
+    height: '80vh',
+  };
+  const options = {
+    styles: mapStyles,
+  };
+  const center = {
+    lat: -37.813629,
+    lng: 144.963058,
+  };
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if ( loadError ) return "Error loading Maps";
+  if ( !isLoaded ) return "Loading Maps";
+
+  return (
+    <div className="map">
+      <GoogleMap
+        mapContainerStyle={ mapContainerStyle }
+        zoom={ 12 }
+        center={ center }
+        options={ options }
+      ></GoogleMap>
+    </div>
+  );
+};
 
 export default Map;
