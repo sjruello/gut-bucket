71029161806c6ceaa0dfb8a38dfe1e6025553b8b
Sarah <sarahjune@gmail.com>
2021-11-14 15:20:39 +1100

    mytrip accordion

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-
 import { auth, signInWithGoogle } from "../../firebase/firebase.js";
 
 import "./sign-in.styles.scss";
--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -1,15 +1,104 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
 
 import "./dashboard.styles.scss";
 
-const Dashboard = () => (
-  <div className="homepage">
-    <h2>MY TRIPS</h2>
-    <Link className="option" to="/trip/1">
-      MELBOURNE 12-12-2021
-    </Link>
-  </div>
-);
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
 
-export default Dashboard;
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark" ? "rgba(255, 255, 255, .05)" : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
+  return (
+    <div className="main-container">
+      <div className="trip-accordions">
+        <Accordion expanded={expanded === "panel1"} onChange={handleChange("panel1")}>
+          <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+            <Typography>Trip #1 - Melbourne</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit
+              amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === "panel2"} onChange={handleChange("panel2")}>
+          <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+            <Typography>Trip #2 - Sydney</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit
+              amet blandit leo lobortis eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion expanded={expanded === "panel3"} onChange={handleChange("panel3")}>
+          <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+            <Typography>Trip #3 - Adelaide</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <Link to="/trip/1">
+                <button variant="contained">Open Trip</button>
+              </Link>
+              <div className="venues-show">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit
+                amet blandit leo lobortis eget.
+              </div>
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </div>
+  );
+}
