5f28ff342e63b993c528a647fb99e2dd28e34f15
Sarah <sarahjune@gmail.com>
2021-11-17 17:38:24 +1100

    rollback

--- src/components/trip-preview/trip-preview.component.jsx
+++ src/components/trip-preview/trip-preview.component.jsx
@@ -3,32 +3,36 @@ import { getUserTrip, getVenues } from "../../firebase/firebase";
 import "./trip-preview.styles.scss";
 
 const TripPreview = ({ userID, tripID, tripVenues = null }) => {
-  const [venues, setVenues] = useState([]);
+  // const [venues, setVenues] = useState([]);
 
-  useEffect(() => {
-    getVenues(userID, tripID)
-      .get()
-      .then((querySnapshot) => {
-        let tripVenues = [];
-        querySnapshot.forEach((doc) => {
-          tripVenues.push([
-            doc.id,
-            doc.data().name,
-            doc.data().address,
-            doc.data().image,
-          ]);
-        });
-        setVenues(tripVenues);
-      });
-  }, [userID, tripID, venues]);
+  // useEffect(() => {
+  //   getVenues(userID, tripID)
+  //     .get()
+  //     .then((querySnapshot) => {
+  //       let tripVenues = [];
+  //       querySnapshot.forEach((doc) => {
+  //         tripVenues.push([
+  //           doc.id,
+  //           doc.data().name,
+  //           doc.data().address,
+  //           doc.data().image,
+  //         ]);
+  //       });
+  //       setVenues(tripVenues);
+  //     });
+  // }, [userID, tripID]);
+
+  if (!tripVenues) {
+    return "";
+  }
 
   return (
     <div>
-      {venues.length === 0 ? (
+      {tripVenues.length === 0 ? (
         <h2>No Venues Added!</h2>
       ) : (
         <div id="venue-box">
-          {venues.map((venue, index) => (
+          {tripVenues.map((venue, index) => (
             <div className="mini-venue-card" key={index}>
               <span className="venue-title">{venue[1]}</span>
               <div
